afb8b3b614fff25aab15aba285cf976ec8f64443
Joshua Welsh <j22yosh@gmail.com>
2022-06-30 19:04:42 -0400

    4th commit

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import { uuid } from 'uuidv4'
 
-function Form() {
+function Form({setTasks, tasks}) {
+  const [entry, setEntry] = useState("");
+ 
+  function handleAdd(e) {
+    e.preventDefault();
+    const newTask = {description: entry, id: uuid() }
+    setTasks([...tasks, newTask]);
+    console.log(e);
+  }
+  function handleDel(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div>Form</div>
-  )
+    <form>
+      <input
+        type="text"
+        onChange={(event) => {
+          setEntry(event.target.value);
+          console.log(entry);
+        }}
+      ></input>
+      <button onClick={handleAdd}>Add item</button>
+      <button onClick={handleDel}>Remove item</button>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
