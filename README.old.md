539a2542ec8cf9279baabf3d5f0abe771d3f3482
Joshua Welsh <j22yosh@gmail.com>
2022-06-30 12:13:30 -0400

    third commit

--- README.old.md
+++ README.old.md
@@ -1,1 +1,2 @@
 # phase-2-project
+`Run json server cmd- json-server --watch db.json -p 3001`
--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Header from './Header';
+import TaskList from './TaskList';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
+      <Header title="To-do App"/>
+      
+      
       <Routes>
+        <Route path="/task-list" element={<TaskList />}/>
+        <Route path="/add-task"  element={<Form />}/>
         <Route path="/" element={<Home />}/>
       </Routes>
     </div>
