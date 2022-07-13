f6e301417f77fdd787b2702b2a11c6586b5a10b5
Joshua Welsh <j22yosh@gmail.com>
2022-07-13 10:33:00 -0400

    get request working

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 // import logo from './logo.svg';
-import data from "/home/j22yosh/Development/code/phase2/phase-2-project/src/db.json"
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
+
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 import Header from "./components/Header";
-import TaskList from './components/TaskList';
-import About from './components/About';
-import { useState } from 'react';
-import NavBar from './components/NavBar';
-import '/home/j22yosh/Development/code/phase2/phase-2-project/src/App.css'
+import TaskList from "./components/TaskList";
+import About from "./components/About";
+import { useState, useEffect } from "react";
+import NavBar from "./components/NavBar";
+import "/home/j22yosh/Development/code/phase2/phase-2-project/src/App.css";
 
 function App() {
-  const[tasks, setTasks] = useState(data)
+
+  useEffect (() =>  {fetch ("http://localhost:8000/tasks")
+  .then(res => res.json())
+  .then(data => setTasks(data))},[]);
 
   
+  const [tasks, setTasks] = useState([]);
+
   
 
   return (
     <div className="App">
-      <Header title="To-do App"/>
+      <Header title="To-do App" />
       <NavBar />
-      
+
       <Routes>
-        <Route path="/task-list" element={<TaskList setTasks={setTasks}tasks={tasks}/>}/>
-        <Route path="/about"  element={<About />}/>
-        <Route path="/" element={<Home />}/>
+        <Route
+          path="/task-list"
+          element={<TaskList setTasks={setTasks} tasks={tasks} />}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
